1915e10b558c2e42dfd501d53f8b0fcb1fe8e237
Ben Barlow <ben.barlow@yahoo.co.uk>
2022-12-14 16:45:38 +0000

    first commit

---
+++ console.py
@@ -0,0 +1,33 @@
+import pdb
+from models.author import Author
+from models.book import Book
+
+import repositories.author_repository as author_repository
+import repositories.book_repository as book_repository
+
+book_repository.delete_all()
+author_repository.delete_all()
+
+author_1 = Author("John")
+author_repository.save(author_1)
+
+author_test = author_repository.select_all()
+for author in author_test:
+    print(author.__dict__)
+
+single_author = author_repository.select(author_1.id)
+print(single_author.__dict__)
+
+
+book_1 = Book("The Art of Jam Rolls", author_1)
+book_repository.save(book_1)
+
+book_test = book_repository.select_all()
+for book in book_test:
+    print(book.__dict__)
+
+single_book = book_repository.select(book_1.id)
+print(single_book.__dict__)
+
+
+
---
+++ repositories/author_repository.py
@@ -0,0 +1,41 @@
+from db.run_sql import run_sql
+
+from models.author import Author
+
+def save(author):
+    sql = "INSERT INTO authors (name) VALUES (%s) RETURNING *"
+    values = [author.name]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    author.id = id
+    return author
+
+def select_all():
+    authors = []
+    sql = "SELECT * FROM authors"
+    results = run_sql(sql)
+
+    for row in results:
+        author = Author(row['name'], row['id'])
+        authors.append(author)
+    return authors
+
+def select(id):
+    author = None
+    sql = "SELECT * FROM authors WHERE id = %s"
+    values = [id]
+    results = run_sql(sql, values)[0]
+    if results is not None:
+        author = Author(results['name'], results['id'])
+    return author
+
+
+def delete_all():
+    sql = "DELETE FROM authors"
+    run_sql(sql)
+
+def delete(id):
+    sql = "DELETE FROM authors WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
+
